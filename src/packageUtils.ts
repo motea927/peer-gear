050478cd6d3eaed9096c654308b69bb14efeabd5
motea927 <motea927@yahoo.com.tw>
2024-02-16 11:32:36 +0800

    feat(packageUtils): add resolvePackageDir function

--- src/packageUtils.ts
+++ src/packageUtils.ts
@@ -1,3 +1,33 @@
+import resolve from "resolve";
+import type { SyncOpts } from "resolve";
+
+interface PackageJson {
+  name: string;
+  version: string;
+  dependencies: {
+    [key: string]: string;
+  };
+  devDependencies: {
+    [key: string]: string;
+  };
+  peerDependencies: {
+    [key: string]: string;
+  };
+  // deprecated: use peerDependenciesMeta.foo.dev
+  peerDevDependencies: string[];
+  // See: https://github.com/yarnpkg/rfcs/blob/master/accepted/0000-optional-peer-dependencies.md
+  peerDependenciesMeta: {
+    [key: string]: {
+      optional?: boolean;
+      // non-standard
+      dev?: boolean;
+    };
+  };
+  optionalDependencies: {
+    [key: string]: string;
+  };
+}
+
 export type DependencyType =
   | "dependencies"
   | "devDependencies"
@@ -25,4 +55,43 @@ interface PackageMeta {
   devDependencies: Dependency[];
   optionalDependencies: Dependency[];
   peerDependencies: Dependency[];
+}
+
+type DependencyWalkVisitor = (
+  packagePath: string,
+  packageJson: PackageJson,
+  packageMeta: PackageMeta,
+) => void;
+
+export function resolvePackageDir(basedir: string, packageName: string) {
+  return resolvePackageDirWith(resolve.sync, basedir, packageName);
+}
+
+// for unit testing
+export function resolvePackageDirWith(
+  resolveSync: (packageName: string, options?: SyncOpts) => void,
+  basedir: string,
+  packageName: string,
+) {
+  let packagePath: string | undefined;
+
+  // In resolve() v2.x this callback has a different signature
+  // function packageFilter(pkg, pkgfile, pkgdir) {
+  function packageFilter(pkg: Record<string, any>, pkgdir: string) {
+    if (!packagePath || pkg.version) {
+      packagePath = pkgdir;
+    }
+    return pkg;
+  }
+
+  try {
+    resolveSync(packageName, { basedir, packageFilter });
+  } catch {
+    // resolve.sync throws if no main: is present
+    // Some packages (such as @types/*) do not have a main
+    // As long as we have a packagePath, it's fine
+  }
+
+  // noinspection JSUnusedAssignment
+  return packagePath;
 }
