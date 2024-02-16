f157dc7bb1fc7ee99e179c8640f6515d0f8ffd38
motea927 <motea927@yahoo.com.tw>
2024-02-16 16:24:15 +0800

    feat: add findPossibleResolution function

--- src/solution/solution.ts
+++ src/solution/solution.ts
@@ -1,3 +1,5 @@
+import { execSync } from "node:child_process";
+import semver from "semver";
 import type { Dependency } from "../packageUtils/types";
 
 export interface Resolution {
@@ -28,12 +30,48 @@ export function determineResolutionType(problem: Dependency): string {
   return "install";
 }
 
+function semverReverseSort(a: string, b: string) {
+  const lt = semver.lt(a, b);
+  const gt = semver.gt(a, b);
+  if (!lt && !gt) {
+    return 0;
+  } else if (lt) {
+    return 1;
+  }
+  return -1;
+}
+
 function findPossibleResolution(
-  _packageName: string,
-  _allPeerDeps: Dependency[],
+  packageName: string,
+  allPeerDeps: Dependency[],
 ) {
-  // todo
-  return "";
+  const requiredPeerVersions = allPeerDeps.filter(
+    (dep) => dep.name === packageName,
+  );
+  // todo: skip this step if only one required peer version and it's an exact version
+  const command = `npm view ${packageName} versions`;
+  let rawVersionsInfo;
+  try {
+    rawVersionsInfo = execSync(command, { stdio: "pipe" }).toString();
+
+    const availableVersions = JSON.parse(
+      rawVersionsInfo.replace(/'/g, '"'),
+    ).sort(semverReverseSort);
+
+    return availableVersions.find((ver: string) =>
+      requiredPeerVersions.every((peerVer) => {
+        return semver.satisfies(ver, peerVer.version, {
+          includePrerelease: true,
+        });
+      }),
+    );
+  } catch (error) {
+    console.error(`Error while running command: '${command}'`);
+    console.error(error);
+    console.error();
+    console.error("npm output:");
+    console.error(rawVersionsInfo);
+  }
 }
 
 export function determineResolutionVersion(
