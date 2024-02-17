2e1fb6a427f702fe5f0e1c45aa060958d55e8c8c
motea927 <motea927@yahoo.com.tw>
2024-02-17 09:48:15 +0800

    chore: add playground

--- playground/index.ts
+++ playground/index.ts
@@ -1,2 +1,8 @@
-// TODO
-console.log("todo playground");
+function runCommand(...args) {
+  process.argv = ["npx", "peer-gear", ...args];
+
+  return import("../src/cli");
+}
+
+// Run the CLI with the any flag
+runCommand(["--findSolutions"]);
