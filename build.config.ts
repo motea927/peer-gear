92d93ccf05152a07e810fc4e2e5c7c2f14c71d5b
motea927 <motea927@yahoo.com.tw>
2024-02-17 09:11:06 +0800

    build: fix build missing cjs file

--- build.config.ts
+++ build.config.ts
@@ -2,11 +2,10 @@ import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
   // If entries is not provided, will be automatically inferred from package.json
-  entries: [
-    // default
-    "./src/index",
-    "./src/cli",
-  ],
+  entries: ["./src/index", "./src/cli"],
+  rollup: {
+    emitCJS: true,
+  },
 
   // Generates .d.ts declaration file
   declaration: true,
